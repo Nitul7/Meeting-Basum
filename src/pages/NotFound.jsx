5b7d28337089cfef5523dafda02cf4c3aba9259e
Risan Basukala <basukalarisan123@gmail.com>
2026-05-05 18:01:16 +0545

    Test commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ function NotFound() {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
       <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page you are looking for does not exis</p>
     </div>
   );
 }
