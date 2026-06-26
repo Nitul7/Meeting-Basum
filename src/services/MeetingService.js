e3551fc0f8dfd25b161ad2f32772b8ba67878063
Pravesh Chapagain <praveshchapagain@Praveshs-MacBook-Pro.local>
2026-06-26 17:30:09 +0545

    Fixed validation

--- src/services/MeetingService.js
+++ src/services/MeetingService.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import api from "./Api";
 
-const API_URL = "http://localhost:4000/meetings";
+const URL = "/meetings/";
 
 export const scheduleMeeting = async (meetingData) => {
-  return await axios.post(API_URL, meetingData);
+  try {
+    console.log("Meeting data: ");
+    console.log(meetingData);
+    const response = await api.post(URL, meetingData);
+    console.log("Response: ");
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error('Error scheduling meeting:', error);
+    throw error;
+  }
 };
